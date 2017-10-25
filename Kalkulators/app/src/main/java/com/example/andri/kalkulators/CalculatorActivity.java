cbe5344ac8b74e4fb8efea394ca0de973db6a6c3
andris.akacenoks <andris.akacenoks@testdevlab.com>
2017-10-26 00:35:26 +0300

    4. Add landscape layout

--- Kalkulators/app/src/main/java/com/example/andri/kalkulators/CalculatorActivity.java
+++ Kalkulators/app/src/main/java/com/example/andri/kalkulators/CalculatorActivity.java
@@ -7,43 +7,138 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class CalculatorActivity extends Activity {
+import static com.example.andri.kalkulators.CalculatorAction.ADDITION;
+import static com.example.andri.kalkulators.CalculatorAction.DIVISION;
+import static com.example.andri.kalkulators.CalculatorAction.MULTIPLICATION;
+import static com.example.andri.kalkulators.CalculatorAction.SUBTRACTION;
+
+public class CalculatorActivity extends Activity implements View.OnClickListener{
 
     public static final int REQUEST_CALCULATOR = 9;
+    private static boolean isClearable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        LinearLayout linearLayout = new LinearLayout(this);
-        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        setContentView(R.layout.calculator_activity_layout);
 
-        // share button
-        Button buttonShare = new Button(this);
-        buttonShare.setText("Share");
-        buttonShare.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                shareResult("This text has been shared!");
-            }
-        });
-        linearLayout.addView(buttonShare);
+        Button buttonZero = findViewById(R.id.btn0);
+        Button buttonOne = findViewById(R.id.btn1);
+        Button buttonTwo = findViewById(R.id.btn2);
+        Button buttonThree = findViewById(R.id.btn3);
+        Button buttonFour = findViewById(R.id.btn4);
+        Button buttonFive = findViewById(R.id.btn5);
+        Button buttonSix = findViewById(R.id.btn6);
+        Button buttonSeven = findViewById(R.id.btn7);
+        Button buttonEight = findViewById(R.id.btn8);
+        Button buttonNine = findViewById(R.id.btn9);
+        Button buttonAdd = findViewById(R.id.btnPlus);
+        Button buttonSubtract = findViewById(R.id.btnMinus);
+        Button buttonMultiply = findViewById(R.id.btnMultiply);
+        Button buttonDivide = findViewById(R.id.btnDivide);
+        Button buttonClear = findViewById(R.id.btnClear);
+        Button buttonPoint = findViewById(R.id.btnPoint);
+        Button buttonEquals = findViewById(R.id.btnEquals);
 
-        // random button
-        Button buttonRandom = new Button(this);
-        buttonRandom.setText("Random");
-        buttonRandom.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                requestRandom();
-            }
-        });
-        linearLayout.addView(buttonRandom);
+        buttonZero.setOnClickListener(this);
+        buttonOne.setOnClickListener(this);
+        buttonTwo.setOnClickListener(this);
+        buttonThree.setOnClickListener(this);
+        buttonFive.setOnClickListener(this);
+        buttonFour.setOnClickListener(this);
+        buttonSeven.setOnClickListener(this);
+        buttonSix.setOnClickListener(this);
+        buttonEight.setOnClickListener(this);
+        buttonNine.setOnClickListener(this);
+        buttonAdd.setOnClickListener(this);
+        buttonSubtract.setOnClickListener(this);
+        buttonMultiply.setOnClickListener(this);
+        buttonDivide.setOnClickListener(this);
+        buttonPoint.setOnClickListener(this);
+        buttonEquals.setOnClickListener(this);
+        buttonClear.setOnClickListener(this);
 
-        setContentView(linearLayout);
+
     }
+
+        @Override
+        public void onClick(View calculatorView) {
+            Calculator calculator = new CalculatorEngine();
+            TextView resultView = findViewById(R.id.txtResult);
+
+            switch (calculatorView.getId()) {
+                case R.id.btnClear:
+                    calculator.clear();
+                    displayResult(calculator, resultView);
+                    break;
+                case R.id.btnEquals:
+                    try {
+                        calculator.putOperand(resultView.getText().toString());
+                        displayResult(calculator, resultView);
+                    } catch (CalculatorExeption e) {
+                        showMessage("Invalid operation");
+                    }
+                    break;
+                case R.id.btnPlus:
+                case R.id.btnMinus:
+                case R.id.btnMultiply:
+                case R.id.btnDivide:
+                    try {
+                        Button buttonOperation = findViewById(calculatorView.getId());
+                        calculator.putOperand(resultView.getText().toString());
+                        showMessage((buttonOperation.getText().toString()));
+
+
+                        switch (buttonOperation.getText().toString()) {
+                            case "+":
+                                calculator.putAction(ADDITION);
+                                break;
+                            case "-":
+                                calculator.putAction(SUBTRACTION);
+                                break;
+                            case "*":
+                                calculator.putAction(MULTIPLICATION);
+                                break;
+                            case "/":
+                                calculator.putAction(DIVISION);
+                                break;
+                            default:
+                                showMessage("Invalid operator");
+                        }
+                        displayResult(calculator, resultView);
+
+                    } catch (CalculatorExeption e) {
+                        showMessage("Invalid operation");
+                    }
+                    break;
+                default:
+                    Button buttonDigit = findViewById(calculatorView.getId());
+                    addToDisplay(buttonDigit.getText().toString(), resultView);
+                    showMessage((buttonDigit.getText().toString()));
+                    showMessage(("Result: "+calculator.getResult()));
+
+            }
+        }
+
+    public void displayResult(Calculator calculator, TextView text) {
+        text.setText(calculator.getResult());
+        makeClearable(true);
+    }
+
+    public void addToDisplay(String addedText, TextView textView){
+        if(getIsClearable()){
+            textView.setText("");
+            makeClearable(false);
+        }
+        else{
+            textView.append(addedText);
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         switch (requestCode) {
@@ -66,6 +161,14 @@ public class CalculatorActivity extends Activity {
         } else {
             showMessage("No random number received");
         }
+    }
+
+    public void makeClearable(boolean isAllowed){
+        isClearable = isAllowed;
+    }
+
+    public boolean getIsClearable(){
+        return isClearable;
     }
 
     public void shareResult(String input){
